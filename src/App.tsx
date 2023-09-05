2df34ada0632cb13a8358aa8392cf94af15e36e1
Jeewaka Manohara <jamesheller.jeewake@gmail.com>
2023-09-05 22:36:54 +0530

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { z } from 'zod';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>First Name</label>
+        <input type='text' />
+        <label>Last Name</label>
+        <input type='text' />
+        <label>Email</label>
+        <input type='email' />
+        <label>Age</label>
+        <input type='number' />
+        <label>Password</label>
+        <input type='password' />
+        <label>Confirm Password</label>
+        <input type='password' />
+      </form>
     </div>
   );
 }
